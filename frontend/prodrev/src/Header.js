140ac86fe999d6c354d889b07eca4d20404f8419
lokeshnowpada <121910319002@gitam.in>
2022-08-31 23:23:29 +0530

    changes in header

--- frontend/prodrev/src/Header.js
+++ frontend/prodrev/src/Header.js
@@ -1,38 +1,21 @@
 import React from 'react'
 import './Header.css'
-import { Button, Form, Row,Col, InputGroup} from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className='header'>
-        <div className="website-name">
-            <Link to="/">
-                <span>Product </span>
-                <span>Review</span>
-            </Link>
-        </div>
-        <div className="search-bar">
-            <Form>
-                <Row>
-                    <Col xs="auto">
-                        <InputGroup className="mb-2">
-                        <Form.Control id="inlineFormInputGroup" placeholder="search product..."/>
-                        <Button variant='light'>Search</Button>
-                        </InputGroup>
-                    </Col>
+            <Link to="/"><p>Product Review</p></Link>
+                        <input placeholder='enter product name'/>
+                        <button>search</button>
                     <div className="sign-up-in">
                         <Link to='/login'>
                             <Button variant='primary'>Log in</Button>
                         </Link>
-                        <span className='or'>or</span>
                         <Link to='/signup'>
-                            <Button variant='success'>Create a new account</Button>
+                            <Button variant='success' style={{'marginLeft':'20px'}}>Create a new account</Button>
                         </Link>
                     </div>
-
-                </Row>
-            </Form>
-        </div>
     </div>
   );
 }
